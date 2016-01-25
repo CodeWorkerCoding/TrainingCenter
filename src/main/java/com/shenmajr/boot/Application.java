2fdcd4c18fd75768a7dfb2d2b7015f634402dd6e
CodeWorkerCoding <nchufujianjian@sina.cn>
2016-01-25 19:03:34 +0800

    Complete Base Flow Next Task is UI

--- src/main/java/com/shenmajr/boot/Application.java
+++ src/main/java/com/shenmajr/boot/Application.java
@@ -1,12 +1,15 @@
 package com.shenmajr.boot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@ComponentScan(basePackages={"com.shenmajr.boot"})
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"com.shenmajr.boot"})
 public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
