5391a4c650cbb61117e8eef9f3bc1314706939b5
CodeWorkerCoding <nchufujianjian@sina.cn>
2016-02-18 18:41:14 +0800

    add README and security user class files.

---
+++ src/main/java/com/shenmajr/boot/domain/security/Role.java
@@ -0,0 +1,14 @@
+package com.shenmajr.boot.domain.security;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+
+	/**
+	 *Created by fujianjian 2016年2月18日
+	 */
+	private static final long serialVersionUID = -5045788230838771234L;
+
+	private String _id;
+	
+}
---
+++ src/main/java/com/shenmajr/boot/domain/security/User.java
@@ -0,0 +1,14 @@
+package com.shenmajr.boot.domain.security;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 *Created by fujianjian 2016年2月18日
+	 */
+	private static final long serialVersionUID = 7527798775726424023L;
+
+	private String _id;
+	
+}
